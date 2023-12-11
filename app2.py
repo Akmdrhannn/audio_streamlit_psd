64b3bac5e92cfbc4bf60b132aee54f738eed99c2
akmdrhan <kh.raihanaf@gmail.com>
2023-12-11 11:54:41 +0700

    update

--- app2.py
+++ app2.py
@@ -1,8 +1,3 @@
-import subprocess
-
-# Install dependencies from requirements.txt
-subprocess.call(['pip', 'install', '-r', 'requirements.txt'])
-
 import streamlit as st
 import pandas as pd
 import librosa
